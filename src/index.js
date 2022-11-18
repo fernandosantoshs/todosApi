81e9d8e5230fd1dda37dcd77ebd2ecdda8e5da1c
fernandosantoshs <fernandosantoshs@gmail.com>
2022-11-18 11:16:55 -0300

    feat: Created route post('/users') and newUser object

--- src/index.js
+++ src/index.js
@@ -1,41 +1,54 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  //checkExistsUserAccount
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+  const id = uuidv4()
+
+  const newUser = {
+    id: id,
+    name: name,
+    username: username,
+    todos: []
+  }
+
+  users.push(newUser)
+  //console.log(users)
+
+  response.send(newUser).status(200)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // GET todos
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // POST todos
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // PUT todos
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // PATCH todos
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  // DELETE todos
 });
 
 module.exports = app;
